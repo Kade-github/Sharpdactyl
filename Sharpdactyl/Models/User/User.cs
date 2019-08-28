cb1298329fc3b558aea2effc62f771605b403e4e
he8dshot <he8dshot@outlook.com>
2019-08-28 15:40:15 +0100

    Server model and create server api calls

--- Sharpdactyl/Models/User/User.cs
+++ Sharpdactyl/Models/User/User.cs
@@ -1,10 +1,4 @@
 ﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static Sharpdactyl.Models.User.UserDataModel;
 
 namespace Sharpdactyl.Models.User
 {
